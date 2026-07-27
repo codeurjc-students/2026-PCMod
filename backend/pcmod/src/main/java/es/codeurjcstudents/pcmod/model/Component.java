d6728d153ba190e796029741669bd55b2dc9fa2e
jaimeeg5 <j.esteban.2023@alumnos.urjc.es>
2026-07-27 14:51:05 +0200

    feat: add endpoint to fetch pc components

--- backend/pcmod/src/main/java/es/codeurjcstudents/pcmod/model/Component.java
+++ backend/pcmod/src/main/java/es/codeurjcstudents/pcmod/model/Component.java
@@ -53,4 +53,60 @@ public class Component {
     this.price = price;
     this.stock = stock;
   }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public ComponentType getType() {
+    return type;
+  }
+
+  public void setType(ComponentType type) {
+    this.type = type;
+  }
+
+  public String getBrand() {
+    return brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
+
+  public Integer getStock() {
+    return stock;
+  }
+
+  public void setStock(Integer stock) {
+    this.stock = stock;
+  }
 }
---
+++ backend/pcmod/src/main/java/es/codeurjcstudents/pcmod/service/ComponentsService.java
@@ -0,0 +1,32 @@
+package es.codeurjcstudents.pcmod.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import es.codeurjcstudents.pcmod.dto.ComponentDTO;
+import es.codeurjcstudents.pcmod.dto.ComponentMapper;
+import es.codeurjcstudents.pcmod.model.Component;
+import es.codeurjcstudents.pcmod.repository.ComponentsRepository;
+
+@Service
+public class ComponentsService {
+
+  private final ComponentsRepository componentRepository;
+
+  @Autowired
+  private ComponentMapper componentMapper;
+
+  public ComponentsService(ComponentsRepository componentRepository) {
+    this.componentRepository = componentRepository;
+  }
+
+  public Page<Component> findAll(Pageable pageable) {
+    return componentRepository.findAll(pageable);
+  }
+
+  public ComponentDTO toDTO(Component component) {
+    return componentMapper.toDTO(component);
+  }
+}
