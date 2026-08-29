31e2ffd760ecf7dee6886399ed4b8bc6feee39ba
jaimeeg5 <j.esteban.2023@alumnos.urjc.es>
2026-08-29 14:17:42 +0200

    feat: add header, footer, and basic CSS

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router";
+import Footer from "~/components/footer";
+import Header from "~/components/header";
 
 export default function Home() {
   return (
     <>
+      <Header />
       <div>
         <Outlet />
       </div>
+      <Footer />
     </>
   );
 }
